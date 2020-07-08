158f54b6808074c1e987b997e878aa1891457157
Justin Condren <justin.l.condren@vanderbilt.edu>
2020-07-08 15:01:32 -0400

    added build folder to github

---
+++ build/precache-manifest.40ead9542f85a8d1b5ce5591b8ba8cfe.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d9e6ea92f61a69642972b027a57ff2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "664b1910920529fcaac6",
+    "url": "/static/css/main.1844f1d3.chunk.css"
+  },
+  {
+    "revision": "f7b7c25d6aa44ad35eab",
+    "url": "/static/js/2.3a38e7f9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.3a38e7f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "664b1910920529fcaac6",
+    "url": "/static/js/main.c1d29a2b.chunk.js"
+  },
+  {
+    "revision": "a8313cf6d4cf35116dcd",
+    "url": "/static/js/runtime-main.dc5e2fae.js"
+  },
+  {
+    "revision": "bd1c18975a0e36866e1ab5aa560255e3",
+    "url": "/static/media/bracelet.bd1c1897.jpg"
+  },
+  {
+    "revision": "3a1df6c1e0644b8b1a12219346c3f013",
+    "url": "/static/media/curvy.3a1df6c1.png"
+  },
+  {
+    "revision": "4cffa739f751f90686ec3ac6e9b56e0d",
+    "url": "/static/media/icon-check.4cffa739.svg"
+  },
+  {
+    "revision": "0d1d59c2a582fad48c094028a119bd4f",
+    "url": "/static/media/logo.0d1d59c2.png"
+  },
+  {
+    "revision": "148bdab38ffe17ce2b5631a9b4ae701d",
+    "url": "/static/media/warrior.148bdab3.png"
+  }
+]);
