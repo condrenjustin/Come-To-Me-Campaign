4f30e32bab931d8fda41250bf6c2f1d174b64d57
condrenjustin <51341538+condrenjustin@users.noreply.github.com>
2020-07-14 12:10:20 -0400

    changed routing of interactive resource guide

--- src/component/home/ProductCTA.js
+++ src/component/home/ProductCTA.js
@@ -84,11 +84,11 @@ function ProductCTA(props) {
                 <Typography variant="h5">
                   Take our interactive quiz to find curated resources.
                 </Typography>
-                <Link to='/interactiveResourceGuide'>
-                  <Button type="submit" color="primary" variant="contained" className={classes.button}>
+//                 <Link to='/interactiveResourceGuide'>
+                  <Button type="submit" color="primary" variant="contained" href="https://forms.gle/iQQPooUPcmDyyzTG7" className={classes.button}>
                     Take Quiz
                   </Button>
-                </Link>
+//                 </Link>
               </form>
             </div>
         </Grid>
